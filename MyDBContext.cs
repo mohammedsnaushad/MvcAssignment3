11f54bdfa82f5224e88caf0dd724a853685f4dfe
mohammedsnaushad <102455221+mohammedsnaushad@users.noreply.github.com>
2022-05-02 16:18:08 +0530

    Add files via upload

---
+++ MyDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Assignment3.Models;
+
+namespace Assignment3.Models
+{
+ 
+
+    public class MyDBContext :DbContext
+    {
+        public MyDBContext()
+         : base("name=conn")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
